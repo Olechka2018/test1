37a0af9a09297881e0c830fa1355d7b035c9609b
Olechka2018 <58590796+Olechka2018@users.noreply.github.com>
2020-02-01 17:19:02 +0700

    qwer

---
+++ Assets/ButtonHandle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandle : MonoBehaviour
+{
+   
+    public void SetText (string mytext)
+    {
+       Text txt = transform.Find("Text").GetComponent<Text>();
+       txt.text = mytext;
+    }
+
+   
+}
